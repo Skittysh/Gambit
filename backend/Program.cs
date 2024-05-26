e72f05ad5a5efea7e393118f825b0a61bd23bd15
Wetherish <bartikozlowski@gmail.com>
2024-05-26 23:46:52 +0200

    move poc to console app

--- backend/Program.cs
+++ backend/Program.cs
@@ -51,31 +51,3 @@ app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
 
-// using GambitLogic;
-
-// class Program
-// {
-//     private static Game game = new Game();
-//     private static ConsoleInputs inputs = new ConsoleInputs();
-
-//     static void Main()
-//     {
-//         while (true)
-//         {
-//             inputs.GetUserInput();
-//             if (inputs.userInput == "1")
-//             {
-//                 game.Deal();
-//                 game.Display();
-//             }
-//             else if (inputs.userInput == "2")
-//             {
-//                 inputs.GetCardInput();
-//                 game.Play(int.Parse(inputs.userInput));
-//                 Console.WriteLine($"Score: {game.score}");
-//             }
-            
-//         }
-
-//     }
-// }
---
+++ poc/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information// using GambitLogic;
+
+using GambitLogic;
+
+class Program
+{
+    private static Game game = new Game();
+    private static ConsoleInputs inputs = new ConsoleInputs();
+
+    static void Main()
+    {
+        while (true)
+        {
+            inputs.GetUserInput();
+            if (inputs.userInput == "1")
+            {
+                game.Deal();
+                game.Display();
+            }
+            else if (inputs.userInput == "2")
+            {
+                inputs.GetCardInput();
+                game.Play(int.Parse(inputs.userInput));
+                Console.WriteLine($"Score: {game.score}");
+            }
+            
+        }
+
+    }
+}
